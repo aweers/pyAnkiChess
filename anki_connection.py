b1899036ff51482e60054635cffec83591d90302
Alexander Weers <mail@aweers.de>
2022-09-17 18:01:38 +0200

    AnkiChessConnection class

--- anki_connection.py
+++ anki_connection.py
@@ -1,33 +1,61 @@
 import genanki
 
-# Unique for this model
-MODEL_GUID = 1238229309
+class AnkiChessNote(genanki.Note):
+    @property
+    def guid(self):
+        # use move history
+        return genanki.guid_for(self.fields[3])
 
-my_model = genanki.Model(
-    MODEL_GUID,
-    'Simple Model',
-    fields=[
-        {'name': 'Question'},
-        {'name': 'Answer'},
-    ],
-    templates=[
-        {
-        'name': 'Card 1',
-        'qfmt': '{{Question}}',
-        'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
-        },
-    ]
-)
+class AnkiChessConnection:
+    # Unique for this model
+    MODEL_GUID = 1238229309
+    DECK_GUID = 2104873761
 
-my_note = genanki.Note(
-    model=my_model,
-    fields=['Capital of Argentina', 'Buenos Aires']
-)
+    def __init__(self, deck_name, title):
+        self.deck_name = deck_name
+        self.title = title
+        self.deck = genanki.Deck(AnkiChessConnection.DECK_GUID, deck_name)
+        self.model = genanki.Model(
+            AnkiChessConnection.MODEL_GUID,
+            'Simple Model',
+            fields=[
+                {'name': 'Title'},
+                {'name': 'Question'},
+                {'name': 'Answer'},
+                {'name': 'Move history'},
+                {'name': 'URL'}
+            ],
+            templates=[
+                {
+                'name': 'Card 1',
+                'qfmt': '{{Title}} </br> {{Question}}',
+                'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+                },
+            ]
+        )
 
-my_deck = genanki.Deck(
-  2104873761,
-  'Country Capitals')
+    def addCard(self, front_svg, back_svg, move_history, url=""):
+        note = AnkiChessNote(
+            model=self.model,
+            fields=[
+                self.title,
+                front_svg,
+                back_svg,
+                move_history,
+                url
+            ])
+        self.deck.add_note(note)
 
-my_deck.add_note(my_note)
+    def savePackage(self, output):
+        genanki.Package(self.deck).write_to_file(output)
 
-genanki.Package(my_deck).write_to_file('output.apkg')
+anki = AnkiChessConnection("Chess opening", "Vienna Gambit")
+
+with open("studyMove3.svg", "r") as f:
+    svg1 = f.read()
+with open("chessboard.svg", "r") as f:
+    svg2 = f.read()
+
+anki.addCard(svg1, svg2, "e4 e5 Nf3", "www.com")
+
+anki.savePackage("output.apkg")
